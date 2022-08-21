098f180d30e1506c5ae181ac43142d74dbd6bd04
anniemahlberg <amahlberg4@gmail.com>
2022-08-21 11:30:42 -0500

    test

--- db/seed.js
+++ db/seed.js
@@ -36,14 +36,14 @@ try {
     await client.query(`
         CREATE TABLE users (
             id SERIAL PRIMARY KEY,
-            admin BOOLEAN DEFAULT false
+            admin BOOLEAN DEFAULT false,
             username varchar(255) UNIQUE NOT NULL,
             password varchar(255) NOT NULL,
             firstname varchar(255) NOT NULL,
             lastname varchar(255) NOT NULL,
             email varchar(255) NOT NULL,
             venmo varchar(255) NOT NULL,
-            active BOOLEAN DEFAULT true,
+            active BOOLEAN DEFAULT true
         );
     `);
 
