bbaa6fcbbdd5fa104f13a71e20ff96d930e84a5d
anniemahlberg <amahlberg4@gmail.com>
2022-09-01 14:06:55 -0500

    heroku

--- api/picks.js
+++ api/picks.js
@@ -1,5 +1,15 @@
 const express = require('express');
-const { getAllPicks, getPickById, updatePick, createPick, getWeeklyPickByUsername, getAllWeeklyPicks, createWeeklyPick, getWeeklyPickById, getGameById, updateWeeklyPick, getAllActiveWeeklyPicks, deletePick} = require('../db');
+const { getAllPicks, 
+        getPickById, 
+        updatePick, 
+        createPick, 
+        getWeeklyPickByUsername, 
+        createWeeklyPick, 
+        getWeeklyPickById, 
+        getGameById, 
+        updateWeeklyPick, 
+        getAllActiveWeeklyPicks, 
+        deletePick} = require('../db');
 const { requireUser, requireAdmin } = require('./utils');
 const picksRouter = express.Router();
 
